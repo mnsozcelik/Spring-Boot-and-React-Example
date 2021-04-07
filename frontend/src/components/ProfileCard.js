87ec6e861b62575a66465e58c2636fbae125fe50
Mustafa Enes Özçelik <enesozcelik38@gmail.com>
2021-04-07 19:48:14 +0300

    User Page Edit Transactions Implementation

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -1,22 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
 
-
-import ProfileImageWithDefault from './ProfileImageWithDefault';
+import ProfileImageWithDefault from "./ProfileImageWithDefault";
+import Input from "./Input";
+import { updateUser } from "../api/apiCalls";
+import { useApiProgress } from "../shared/ApiProgress";
+import ButtonWithProgress from "./ButtonWithProgress";
 
 const ProfileCard = (props) => {
+  const [inEditMode, setInEditMode] = useState(false);
+  const [updatedDisplayName, setUpdatedDisplayName] = useState();
   const { username: loggedInUsername } = useSelector((store) => ({
     username: store.username,
   }));
   const routeParams = useParams();
   const pathUsername = routeParams.username;
-  const { user } = props;
+  const [user, setUser] = useState({});
+  const [editable, setEditable] = useState(false);
+
+  useEffect(() => {
+    setUser(props.user);
+  }, [props.user]);
+
+  useEffect(() => {
+    setEditable(pathUsername === loggedInUsername);
+  }, [pathUsername, loggedInUsername]);
+
   const { username, displayName, image } = user;
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    if (!inEditMode) {
+      setUpdatedDisplayName(undefined);
+    } else {
+      setUpdatedDisplayName(displayName);
+    }
+  }, [inEditMode, displayName]);
+
+  const onClickSave = async () => {
+    const body = {
+      displayName: updatedDisplayName,
+    };
+    try {
+      const response = await updateUser(username, body);
+      setInEditMode(false);
+      setUser(response.data);
+    } catch (error) {}
+  };
+
+  const pendingApiCall = useApiProgress("put", "/api/1.0/users/" + username);
+
   return (
     <div className="card text-center">
       <div className="card-header ">
-        <h3>{displayName}</h3>
+        {!inEditMode && <h3>{displayName}</h3>}
+        {inEditMode && (
+          <div>
+            <Input
+              label={t("Change Display Name")}
+              defaultValue={displayName}
+              onChange={(event) => {
+                setUpdatedDisplayName(event.target.value);
+              }}
+            />
+          </div>
+        )}
       </div>
       <div className="card-body p-0 ">
         <div className="row p-0">
@@ -33,6 +83,47 @@ const ProfileCard = (props) => {
             <h4>@{username}</h4>
           </div>
         </div>
+      </div>
+      <div className="card-footer text-right">
+        {editable && !inEditMode && (
+          <button
+            type="button"
+            className="btn btn-info btn-rounded d-inline-flex rounded-pill waves-effect"
+            onClick={() => setInEditMode(true)}
+          >
+            <span className="material-icons" aria-hidden="true">
+              edit
+            </span>
+            {t("Edit")}
+          </button>
+        )}
+        {inEditMode && (
+          <div>
+            <ButtonWithProgress
+              className="btn btn-success btn-rounded d-inline-flex rounded-pill waves-effect"
+              onClick={onClickSave}
+              disabled={pendingApiCall}
+              pendingApiCall={pendingApiCall}
+              text={
+                <>
+                  <i className="material-icons">save</i>
+                  {t("Save")}
+                </>
+              }
+            />
+            <button
+              type="button"
+              className="btn btn-danger btn-rounded d-inline-flex rounded-pill waves-effect ml-3"
+              onClick={() => setInEditMode(false)}
+              disabled={pendingApiCall}
+            >
+              <span className="material-icons" aria-hidden="true">
+                cancel
+              </span>
+              {t("Cancel")}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -5,15 +5,18 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hoaxify.ws.shared.CurrentUser;
 import com.hoaxify.ws.shared.GenericResponse;
+import com.hoaxify.ws.user.vm.UserUpdateVM;
 import com.hoaxify.ws.user.vm.UserVM;
 
 @RestController
@@ -34,12 +37,18 @@ public class UserController {
 	Page<UserVM> getUsers(Pageable page, @CurrentUser User user) {
 		return userService.getUsers(page, user).map(UserVM::new);
 	}
-	
+
 	@GetMapping("/users/{username}")
 	UserVM getUser(@PathVariable String username) {
 		User user = userService.getByUsername(username);
 		return new UserVM(user);
 	}
-	
-}
 
+	@PutMapping("/users/{username}")
+	@PreAuthorize("#username == principal.username") 
+	UserVM updateUser(@RequestBody UserUpdateVM updatedUser, @PathVariable String username) {
+		User user = userService.updateUser(username, updatedUser);
+		return new UserVM(user);
+	}
+
+}
